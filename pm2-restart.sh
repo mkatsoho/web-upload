03aff4dc2bbe10971abf8060d79a14f3ef4249df
陈永刚 <chenyg@yinhe.ht>
2021-03-25 13:20:07 +0800

    bug fix in pm2.sh

--- pm2-restart.sh
+++ pm2-restart.sh
@@ -4,5 +4,6 @@ echo "Example: $0                   # http://yourIp:2222 and save files to /tmp"
 echo "Example: $0 8081 ./upload     # http://yourIp:8081 and save files to ./upload"
 
 pm2 del $NAME &> /dev/null
-pm2 del $NAME &> /dev/null
+pm2 del $NAME &> /dev/nulll
+cd `dirname $0`
 pm2 start yarn  --name $NAME -- start $*
